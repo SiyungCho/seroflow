cd6470cc3b6b7342f899046c0a815d1af012804e
siyungcho <20fsc@queensu.ca>
2025-03-04 16:11:48 -0500

    Start Pypeline classes

---
+++ src/pypeline/Wrappers/wrappers.py
@@ -0,0 +1,32 @@
+import os
+import sys
+
+from functools import wraps
+import time
+from Exception import base_exception
+
+def timer(func):
+    @wraps(func)
+    def wrap(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+
+        args[0].logger.info(f"""{func.__name__} took: {end-start} sec """)
+        return result
+    return wrap
+
+def log_error(err_msg, logger, log_only = False):
+    def log_error_inner(func):
+        @wraps(func)
+        def wrap(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except Exception as e:
+                tb_last_frame = traceback.extract_tb(e.__traceback__)[-1]
+                filename, linenum, functioname, codeline = tb_last_frame
+                logger.error(f"""Error {err_msg} ; Occured at: {functioname} ; On line number: {codeline} ; Exception {e} """)
+                if not log_only:
+                    raise Exception(f"""{e}; {err_msg} """)
+            return wrap
+        return log_error_inner
---
+++ src/pypeline/pypeline.py
@@ -0,0 +1,78 @@
+from collections import OrderedDict
+
+# from Log.log_base import *
+from Wrappers.wrappers import *
+# from Utils.utils import *
+# from Types.type_validation import *
+# from Cache.LFUCache import CustomLFUCache
+# from Step.step import step as base_step
+# from Context.context import context as base_context
+
+class pypeline():
+    def __init__(self, mode = "TEST", cache_type = "LFU"):
+        # self.logger = custom_logger().logger
+        self.mode = mode
+        self.__target_extractor = None
+        self.__target_loader = None
+        
+        # init_cache(cache_type)
+
+        self.__parameter_index = {}
+        self.__step_index = OrderedDict()
+        self.__step_key_index = OrderedDict()
+        self.__dataframe_index = {}
+        self.__cache_index = {}
+
+    @property
+    def target_extractor(self):
+        return self.__target_extractor
+    
+    @property
+    def target_loader(self):
+        return self.__target_loader
+    
+    @property
+    def parameter_index(self):
+        return self.__parameter_index
+
+    @property
+    def step_index(self):
+        return self.__step_index
+    
+    @property
+    def step_key_index(self):
+        return self.__step_key_index
+    
+    @property
+    def dataframe_index(self):
+        return self.__dataframe_index
+    
+    @property
+    def cache_index(self):
+        return self.__cache_index
+    
+    @target_extractor.setter
+    def target_extractor(self, extractor):
+        if is_extractor(extractor, True):
+            self.__target_extractor = extractor
+            self.logger.info("Target extractor set...")
+        return
+    
+    @target_loader.setter
+    def target_loader(self, loader):
+        if is_loader(loader, True):
+            self.__target_loader = loader
+            self.logger.info("Target loader set...")
+        return
+    
+    def update_parameter_index(self, parameter, value):
+        self.parameter_index[parameter] = value
+        return
+    
+
+    # def init_cache(cache_type):
+    #     match cache_type:
+    #         case "LFU":
+    #             self.__cache = CustomLFUCache
+    #         case _:
+    #             self.__cache = CustomLFUCache
